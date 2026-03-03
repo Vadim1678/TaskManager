a50946379cd4a05843f8b34f92d7851df34a5bb9
Vadim Derish <vadimderis4@gmail.com>
2026-03-03 14:05:16 +0200

    homework 18

---
+++ tasks/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import TaskListView, TaskCreateView
+
+urlpatterns = [
+    path("", TaskListView.as_view(), name="task-list"),
+    path("create/", TaskCreateView.as_view(), name="task-create"),
+]
---
+++ tasks/views.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    ListView,
+)
+
+
+from .models import Task
+from .forms import TaskForm
+
+
+
+
+class TaskListView(LoginRequiredMixin, ListView):
+    model = Task
+    template_name = "tasks/task-list.html"
+    context_object_name = "tasks"
+
+    def get_queryset(self):
+        return Task.objects.filter(owner=self.request.user)
+
+class TaskCreateView(CreateView):
+    model = Task
+    template_name = "tasks/task_create.html"
+    form_class = TaskForm
+    success_url = reverse_lazy("task-list")
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
